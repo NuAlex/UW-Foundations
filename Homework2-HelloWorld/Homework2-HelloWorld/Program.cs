d9bb91694c2e86aade5d430888367e496b08bafa
Nuno Alexandre <nuno.alex@hotmail.com>
2023-04-24 19:53:28 -0700

    Adding all homework and labs

---
+++ Homework2-HelloWorld/Homework2-HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homework2_HelloWorld
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World! Nuno");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab1-Variables/Lab1-Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare a variable named "age" of type "int"
+        int age;
+
+        // Set the value of "age" to 21
+        age = 21; // --> commenting this will cause an "unassigned local variable" error since the var does not have a value assigned.
+
+        Console.WriteLine("My age is {0}", age);
+
+        // increment age by 2
+        age += 2;
+
+        Console.WriteLine("My real age is {0}", age);
+        Console.ReadLine();
+    }
+}
