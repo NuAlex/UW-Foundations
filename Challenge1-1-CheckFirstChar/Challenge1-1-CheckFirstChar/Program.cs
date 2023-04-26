98505384daab64f052fda21db0765031dc8ca270
Nuno Alexandre <nuno.alex@hotmail.com>
2023-04-26 00:12:21 -0700

    update project

--- Challenge1-1-CheckFirstChar/Challenge1-1-CheckFirstChar/Program.cs
+++ Challenge1-1-CheckFirstChar/Challenge1-1-CheckFirstChar/Program.cs
@@ -7,9 +7,11 @@ namespace Challenge1_1_CheckFirstChar
         static void Main(string[] args)
         {
 
+
+            // ---- Write a program that determines if the first character typed in is alphabetic
             char aChar = (char) 65;
             int iChar = aChar;
-            Console.WriteLine("'{1}' is a char #{0}.\n\n", iChar, aChar);
+            Console.WriteLine("EXAMPLE: '{1}' is a char #{0} and is alfanumeric (upper-case letter).\n\n", iChar, aChar);
             
             char keyChar;
             do
@@ -17,7 +19,26 @@ namespace Challenge1_1_CheckFirstChar
                 Console.WriteLine("Press any key to try other characters or press 'X' to exit:");
                 ConsoleKeyInfo keyPress = Console.ReadKey(true);
                 keyChar = keyPress.KeyChar;
-                Console.WriteLine("'{0}' is a char #{1}.", keyChar, (int)keyChar);
+                string alphanumeric;
+
+                if ((int)keyChar > 47 && (int)keyChar <58)
+                {
+                    alphanumeric = "alfanumeric (number)";
+                }
+                else if ((int)keyChar > 64 && (int)keyChar < 91)
+                {
+                    alphanumeric = "alfanumeric (upper-case letter)";
+                }
+                else if ((int)keyChar > 96 && (int)keyChar < 123)
+                {
+                    alphanumeric = "alfanumeric (lower-case letter)";
+                }
+                else
+                {
+                    alphanumeric = "not alfanumeric";
+                }
+
+                Console.WriteLine("'{0}' is a char #{1} and is {2}.", keyChar, (int)keyChar, alphanumeric);
                 Console.WriteLine();
 
             } 
