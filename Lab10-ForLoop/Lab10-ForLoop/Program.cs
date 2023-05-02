e8a9ae4b00c547ea7174a5643f9684bcba8814cf
Nuno Alexandre <nuno.alex@hotmail.com>
2023-05-01 20:32:32 -0700

    Adding Lab9-10

---
+++ Lab10-ForLoop/Lab10-ForLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿// Write a program that calculates the average of numbers given by the user.
+
+using System;
+
+namespace Lab10_ForLoop
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            float sum = 0.0f;
+            float average;
+            string input;
+            int totalNumbers = 10;
+
+            // sequentially add the numbers up from 1 to totalNumbers
+            for (int i = 1; i <= totalNumbers; i++)
+            {
+                Console.Write("Insert number {0} of 10 to sum: ", i);
+                input = Console.ReadLine();
+                sum += float.Parse(input);
+            }
+
+            // finding the average
+            average = sum / totalNumbers;
+            Console.WriteLine("The average of the given {1} numbers is {0}", average, totalNumbers);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab7-FindSmith/Lab7-FindSmith/Program.cs
@@ -0,0 +1,31 @@
+﻿// Hint: Use the string EndsWith method.
+
+using System;
+
+namespace Lab7_FindSmith
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string str;
+            do
+            {
+                Console.Write("Enter a person's full name: ");
+                str = Console.ReadLine();
+                str = str.ToLower();
+
+                if (str.EndsWith(" smith"))
+                {
+                    Console.WriteLine("You found a 'Smith' in '{0}'!", str);
+                }
+                else
+                {
+                    Console.WriteLine("'Smith' not found in '{0}'. Please try again.", str);
+                }
+
+                Console.WriteLine("\nPress CTRL+C to exit.\n");
+            } while (true);
+        }
+    }
+}
