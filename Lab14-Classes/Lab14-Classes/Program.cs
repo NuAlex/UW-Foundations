df0760ceb5c245b77b16d315824e294f03f03006
Nuno Alexandre <nuno.alex@hotmail.com>
2023-05-15 20:22:26 -0700

    Pushing Lab14-15

---
+++ Lab14-Classes/Lab14-Classes/Program.cs
@@ -0,0 +1,58 @@
+﻿// Lab 14 - Classes
+// Define the Contact class
+using System;
+
+namespace Lab14_Classes
+{
+    class Contact
+    {
+        /* Without Data Hiring - bad practice
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        */
+
+        // Best Practice
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName { get { return firstName; } set {  firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        public string GetFullName()
+        {
+            string fullName = string.Format("{0} {1}", FirstName, LastName);
+            return fullName;
+   
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Contact contact = new Contact
+            {
+                FirstName = "Nuno",
+                LastName = "Alex",
+                Age = 47
+            };
+
+            /* Same as:
+            Contact contact = new Contact();
+
+            contact.FirstName = "Nuno";
+            contact.LastName = "Alex";
+            contact.Age = 47;
+            */
+
+            // GetFullName() returns "FirstName LastName"
+            Console.WriteLine("Name: {0} \nAge: {1}", contact.GetFullName(), contact.Age);
+            Console.ReadLine();
+
+        }
+    }
+}
