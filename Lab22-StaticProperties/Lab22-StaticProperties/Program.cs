36f26695b9cf11e0342a758c5fc47bb7509a5f10
Nuno Alexandre <nuno.alex@hotmail.com>
2023-06-05 19:47:34 -0700

    Pushing Session8

---
+++ Lab22-StaticProperties/Lab22-StaticProperties/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Modify the Static Fields lab to create a read-only static property for CurrentNextContactId. It would simply return the current value of nextContactId.
+ * A read-only property only has get and not set.
+ * Use the following code for your Main. Use the struct Contact from your Static Fields lab with the new static property CurrentNextContactId.
+ */ 
+
+using System;
+
+internal class Program
+{
+    struct Contact
+    {
+        public int ContactId;
+        public String Name;
+        public String PhoneNumber;
+
+        public Contact(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            ContactId = nextContactId++;
+        }
+        private static int nextContactId = 100;
+
+        // TODO: Copy your code from Static Fields lab here
+        // TODO: Add static property CurrenctNextId
+        public static int CurrentNextContactId
+        {
+            get => nextContactId;
+        }
+    }
+    static void Main()
+    {
+        Console.WriteLine("CurrentNextContactId = {0} should be 100", Contact.CurrentNextContactId);
+
+        var contact = new Contact("mike", "111-222-3333");
+
+        // TODO: Why does is not compile? What does the compiler say?
+        // >> Static member 'member' cannot be accessed with an instance reference
+        //Console.WriteLine("CurrentNextContactId = {0} should be 101", contact.CurrentNextContactId);
+        // Correction:
+        Console.WriteLine("CurrentNextContactId = {0} should be 101", Contact.CurrentNextContactId);
+
+        var contact2 = new Contact("steve", "222-333-4444");
+
+        // TODO: Why does is not compile? What does the compiler say?
+        // >> Static member 'member' cannot be accessed with an instance reference
+        //Console.WriteLine("CurrentNextContactId = {0} should be 102", contact2.CurrentNextContactId);
+        // Correction:
+        Console.WriteLine("CurrentNextContactId = {0} should be 102", Contact.CurrentNextContactId);
+
+        Console.ReadLine();
+    }
+}
---
+++ Pilot04-Classes/Pilot04-Classes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilot04_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create new object instances
+            Book book1 = new Book("War and Peace", "Tolstoy", 800);
+            Book book2 = new Book("The Grapes of Wrath", "Steinbeck", 400);
+
+            // Call method
+            Console.WriteLine(book1.GetDescription());
+            Console.WriteLine(book2.GetDescription());
+
+            book1.Name = "lala1";
+            book1.Author = "lala2";
+
+            Console.WriteLine(book1.GetDescription());
+            Console.WriteLine(book1.Name);
+
+            Console.WriteLine(book1.Description);
+            
+
+            book2.Price = 12.34m; // 'm' is for decimal values
+            book2.ISBN = "110140449876";
+
+
+            Console.WriteLine(book2.Description);
+            Console.WriteLine(book2.ToString());
+            Console.WriteLine(book2.ToString('B'));
+            Console.WriteLine(book2.ToString('F'));
+
+            Console.ReadLine();
+        }
+    }
+}
